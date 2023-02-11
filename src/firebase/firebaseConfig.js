988f5f53fe3ec0d8dd4ad9e7199742121d81fa03
Anthony Serquén <srquen77@gmail.com>
2023-02-10 19:47:30 -0500

    Intente agregar notificaciones cuando un usuario recibe un mensaje pero no pude pipi, frustracion a mil, seguire intentando despues, lo dejare para el ultimo mejor

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import {getAnalytics} from "firebase/analytics";
+// import {getMessaging} from "firebase/messaging"; 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,14 +13,17 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+// const analytics = getAnalytics();
+// const messaging = getMessaging(app);
 
 
 export {
-  app 
+  app,
+  // messaging
 }
--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -5,15 +5,49 @@ import Sidebar from "./../components/Sidebar";
 import useGetUserLogged from "../hooks/useGetUserLogged";
 import { ChatsProvider } from "../context/chatsContext";
 import {ChatCurrentProvider} from "../context/chatCurrentContext";
+// import { useAuth } from "../context/authContext";
+
+// import {getToken, onMessage} from "firebase/messaging";
+// import {messaging} from "./../firebase/firebaseConfig";
+// import {ToastContainer, toast} from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 const Layout = () => {
     const [navbarisActive, changeNavbarisActive] = useState(false);
     const [sidebarActive, changeSidebarActive] = useState(true);
 
     const user = useGetUserLogged();
 
+    // const usuario = useAuth();
+
+    // console.log(usuario);
+
+    // console.log(messaging)
+    // const activarMensajes = async ()=> {
+    //     const token = await getToken(messaging,{
+    //       vapidKey: "BCY_f-LinFeR5h-f2ireh3tWcJBykXvj-LsozvmrrdPM__64qVmErDSWwd93G9y24tPfQQFh7Vl4VeLiYKhc1d0"
+    //     }).catch(error => console.log("Tuviste un error al generar el token, papá"));
+      
+      
+    //     if(token) console.log("tu token:",token);
+    //     if(!token) console.log("no tienes token, rey");
+    //   }
+
+    //   activarMensajes();
+
+    // useEffect(()=>{
+    //     onMessage(messaging, message=>{
+    //       console.log("tu mensaje:", message);
+    //       toast(message.notification.title);
+        
+    //     })
+        
+        
+    //     }, []);
+
 
     return(
         <ChatsProvider>
+            {/* <ToastContainer /> */}
             <Navbar 
                 navbarisActive={navbarisActive}
                 userLogged={user}
